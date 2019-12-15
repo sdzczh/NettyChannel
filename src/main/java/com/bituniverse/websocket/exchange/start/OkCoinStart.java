77d3c4e6ebdb94810371c9f6ab34fd62c174ba58
赵赫 <zgsdzczh@163.com>
2019-12-15 15:56:54 +0800

    commit

--- src/main/java/com/bituniverse/websocket/exchange/start/OkCoinStart.java
+++ src/main/java/com/bituniverse/websocket/exchange/start/OkCoinStart.java
@@ -34,7 +34,7 @@ public class OkCoinStart {
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/depth5:LTC-USDT\"]}");
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/depth5:XRP-USDT\"]}");
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/depth5:EOS-USDT\"]}");
-        client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/depth5:DOGE-USDT\"]}");
+//        client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/depth5:DOGE-USDT\"]}");
 
         //tiker信息
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/ticker:BTC-USDT\"]}");
@@ -42,7 +42,7 @@ public class OkCoinStart {
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/ticker:LTC-USDT\"]}");
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/ticker:EOS-USDT\"]}");
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/ticker:XRP-USDT\"]}");
-        client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/ticker:DOGE-USDT\"]}");
+//        client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/ticker:DOGE-USDT\"]}");
 
         //成交信息
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/trade:BTC-USDT\"]}");
@@ -50,7 +50,7 @@ public class OkCoinStart {
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/trade:LTC-USDT\"]}");
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/trade:EOS-USDT\"]}");
         client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/trade:XRP-USDT\"]}");
-        client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/trade:DOGE-USDT\"]}");
+//        client.sendText("{\"op\": \"subscribe\", \"args\": [\"spot/trade:DOGE-USDT\"]}");
 
         /*JSONObject jsonObject = new JSONObject();
         JSONArray jsonArray = new JSONArray();
