0dcbf0e7a6baebf7a4cba4f357da2d8731d6d1df
赵赫 <zgsdzczh@163.com>
2020-05-19 21:46:31 +0800

    commit

--- src/main/java/com/bituniverse/websocket/netty/service/OKCoinServiceImpl.java
+++ src/main/java/com/bituniverse/websocket/netty/service/OKCoinServiceImpl.java
@@ -46,9 +46,9 @@ public class OKCoinServiceImpl implements WebSocketService {
                 log.info("收到okcoin服务器数据jsonObject：" + ((JSONObject)json).toJSONString());
             }
             JSONObject jsonObject = JSONObject.parseObject(msg);
-            if("error".equals(jsonObject.getString("event"))){
+           /* if("error".equals(jsonObject.getString("event"))){
                 OkCoinStart.start();
-            }
+            }*/
             String table = jsonObject.getString("table");
             JSONArray data = jsonObject.getJSONArray("data");
             //深度交易挂单订阅
